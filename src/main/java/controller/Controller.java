0770c479548a9bba1e0ede02600b4eec4c6e2a02
Fear <impera.et.conquer@gmail.com>
2017-04-26 15:34:46 +0300

    added tests

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -6,22 +6,21 @@ import model.Notebook;
 import view.View;
 
 public class Controller {
-	
+
 	private View view;
 	private Notebook notebook;
-	
+
 	public Controller(Notebook notebook, View view) {
 		this.view = view;
 		this.notebook = notebook;
 	}
-	
+
 	public void processUser() throws Exception {
 		Scanner sc = new Scanner(System.in);
 		InputController inputController = new InputController(sc, notebook, view);
-		
-		while(true)
-		inputController.addNewNote();
+
+		while (true)
+			inputController.addNewNote();
 	}
-	
-	
+
 }
--- src/test/java/ua/training/task3/PhoneBookTest.java
+++ src/test/java/ua/training/task3/PhoneBookTest.java
@@ -1,13 +1,126 @@
 package ua.training.task3;
 
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import controller.RegEx;
+import controller.ScannerUtil;
+import view.GlobalConstants;
+import view.View;
 
 /**
  * Unit test for simple App.
  */
 public class PhoneBookTest {
+
 	@Test
-	public void testSomething() {
-		
+	public void testRegExName() {
+
+		String wrongdata = "mikhail VLADIMIR 1512515 Fear@gmail.com 0633478412";
+		String rightdata = "Dmitriy";
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(wrongdata).append(GlobalConstants.SPACE).append(rightdata);
+
+		ByteArrayInputStream in = new ByteArrayInputStream(sb.toString().getBytes());
+		System.setIn(in);
+		Scanner sc = new Scanner(System.in);
+
+		ScannerUtil scn = new ScannerUtil(sc, new View());
+
+		String actualResult = scn.getStringValueFromScanner(RegEx.NAME);
+		String expectedResult = rightdata;
+
+		boolean stringValuesAreThesame = false;
+		stringValuesAreThesame = expectedResult.equals(actualResult);
+
+		System.setIn(System.in);
+
+		Assert.assertTrue(stringValuesAreThesame);
+
 	}
+
+	@Test
+	public void testRegExPhone() {
+
+		String wrongdata = "mikhail VLADIMIR 1512515 Fear@gmail.com 0633478125412";
+		String rightdata = "0668521440";
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(wrongdata).append(GlobalConstants.SPACE).append(rightdata);
+
+		ByteArrayInputStream in = new ByteArrayInputStream(sb.toString().getBytes());
+		System.setIn(in);
+		Scanner sc = new Scanner(System.in);
+
+		ScannerUtil scn = new ScannerUtil(sc, new View());
+
+		String actualResult = scn.getStringValueFromScanner(RegEx.PHONE_NUMBER);
+		String expectedResult = rightdata;
+
+		boolean stringValuesAreThesame = false;
+		stringValuesAreThesame = expectedResult.equals(actualResult);
+
+		System.setIn(System.in);
+
+		Assert.assertTrue(stringValuesAreThesame);
+	}
+
+	@Test
+	public void testRegExMail() {
+
+		String wrongdata = "mikhail VLADIMIR 1512515 Fear^gmail.com 0633478412";
+		String rightdata = "fear@gmail.com";
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(wrongdata).append(GlobalConstants.SPACE).append(rightdata);
+
+		ByteArrayInputStream in = new ByteArrayInputStream(sb.toString().getBytes());
+		System.setIn(in);
+		Scanner sc = new Scanner(System.in);
+
+		ScannerUtil scn = new ScannerUtil(sc, new View());
+
+		String actualResult = scn.getStringValueFromScanner(RegEx.EMAIL);
+		String expectedResult = rightdata;
+
+		boolean stringValuesAreThesame = false;
+		stringValuesAreThesame = expectedResult.equals(actualResult);
+
+		System.setIn(System.in);
+
+		Assert.assertTrue(stringValuesAreThesame);
+
+	}
+
+	@Test
+	public void testRegExSkype() {
+
+		String wrongdata = "15mikhail 2323VLADIMIR 1512515 242Fear@gmail.com 0633478412";
+		String rightdata = "fearformeh";
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(wrongdata).append(GlobalConstants.SPACE).append(rightdata);
+
+		ByteArrayInputStream in = new ByteArrayInputStream(sb.toString().getBytes());
+		System.setIn(in);
+		Scanner sc = new Scanner(System.in);
+
+		ScannerUtil scn = new ScannerUtil(sc, new View());
+
+		String actualResult = scn.getStringValueFromScanner(RegEx.SKYPE);
+		String expectedResult = rightdata;
+
+		boolean stringValuesAreThesame = false;
+		stringValuesAreThesame = expectedResult.equals(actualResult);
+
+		System.setIn(System.in);
+
+		Assert.assertTrue(stringValuesAreThesame);
+
+	}
+
 }
